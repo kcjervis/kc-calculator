9f89d09ed8aef444ee815f67c97ef94277e9ed22
まどの <madonoharu@gmail.com>
2019-10-20 21:03:58 +0900

    add tests

---
+++ src/tests/AirControlState.spec.ts
@@ -0,0 +1,32 @@
+import { AirControlState } from "../constants"
+
+describe("AirControlState", () => {
+  it("getBoundaryValues", () => {
+    expect(AirControlState.getBoundaryValues(7)).toMatchObject({
+      AirSupremacy: 21,
+      AirSuperiority: 11,
+      AirParity: 5,
+      AirDenial: 3
+    })
+    expect(AirControlState.getBoundaryValues(30)).toMatchObject({
+      AirSupremacy: 90,
+      AirSuperiority: 45,
+      AirParity: 21,
+      AirDenial: 11
+    })
+  })
+
+  it("fromFighterPower", () => {
+    expect(AirControlState.fromFighterPower(90, 30)).toBe(AirControlState.AirSupremacy)
+    expect(AirControlState.fromFighterPower(89, 30)).toBe(AirControlState.AirSuperiority)
+
+    expect(AirControlState.fromFighterPower(45, 30)).toBe(AirControlState.AirSuperiority)
+    expect(AirControlState.fromFighterPower(44, 30)).toBe(AirControlState.AirParity)
+
+    expect(AirControlState.fromFighterPower(21, 30)).toBe(AirControlState.AirParity)
+    expect(AirControlState.fromFighterPower(20, 30)).toBe(AirControlState.AirDenial)
+
+    expect(AirControlState.fromFighterPower(11, 30)).toBe(AirControlState.AirDenial)
+    expect(AirControlState.fromFighterPower(10, 30)).toBe(AirControlState.AirIncapability)
+  })
+})
---
+++ src/types/sift.d.ts
@@ -0,0 +1,67 @@
+declare module "sift" {
+  export type SupportedType = string | { [index: string]: RegExp | any } | number | null | any
+  export type KeyOrValue<T extends SupportedType> = T
+
+  export type ElemMatch<T> = { [P in keyof T]?: SiftQuery<T[P]> }
+
+  export type Query<T extends SupportedType> = {
+    $eq?: T
+    $ne?: T
+    $or?: T[]
+    $gt?: T
+    $gte?: T
+    $lt?: T
+    $lte?: T
+    $mod?: number[]
+    $in?: T[]
+    $nin?: T[]
+    $not?: SiftQuery<T>
+    $type?: any
+    $all?: T[]
+    $size?: number
+    $nor?: T[]
+    $and?: T[]
+    $regex?: RegExp | string
+    $elemMatch?: ExternalQuery<T>
+    $exists?: boolean
+    $where?: string | WhereFn<T>
+    $options?: "i" | "g" | "m" | "u"
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface InternalQuery<T extends SupportedType> extends Query<T> {}
+
+  export type ExternalQuery<T extends SupportedType> = ElemMatch<T>
+
+  export type WhereFn<T extends SupportedType> = (this: T, value: T, index: number, array: T) => boolean
+
+  export type FilterFn = <T>(value: T, index?: number, array?: T[]) => boolean
+
+  type SiftQueryBase<T extends SupportedType> = ExternalQuery<T> | InternalQuery<T>
+
+  export type SiftQuery<T extends SupportedType> = T extends Array<infer U>
+    ? SiftQueryBase<U> | SiftQueryBase<T>
+    : SiftQueryBase<T>
+
+  export type PluginDefinition<T> = {
+    [index: string]: (a: T, b: T) => boolean | number
+  }
+
+  export type PluginFunction<T> = (sift: Sift) => PluginDefinition<T>
+
+  export type Exec = <T extends SupportedType>(array: T) => T
+
+  type Options<T> = {
+    expressions?: {
+      [identifier: string]: (item: T, query: SiftQuery<T>, options: Options<T>) => boolean
+    }
+  }
+
+  export interface Sift {
+    <T extends SupportedType>(query: SiftQuery<T>, options?: Options<T>): FilterFn
+    compare<T, K>(a: T, b: K): 0 | -1 | 1
+  }
+
+  const sift: Sift
+  export default sift
+}
